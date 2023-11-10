720f7bb7a3682c7b24049dd79d80ebc66f7fdf95
Kristof Toth <toth.kristof111@gmail.com>
2023-11-10 17:12:49 +0100

    More interesting test scene

--- Editor/src/TestSystem.hpp
+++ Editor/src/TestSystem.hpp
@@ -9,11 +9,27 @@
 class TestSystem : public System<TestSystem>
 {
 public:
-	TestSystem(EntityID entity) : m_entity(entity){}
+	TestSystem(){}
 	void FixedUpdate(double dt) override
 	{
-		m_scene.ecs->componentManager->GetComponent<Transform>(m_entity)->pos = glm::vec3(10,0,0) * (float)glm::sin(Time::GetTime() * 20);		
+        float distance = 5;
+        float speed    = 10;
+        float s        = glm::sin((float)Time::GetTime());
+		for (auto* light : m_scene.ecs->componentManager->GetComponents<PointLight>())
+		{
+            auto* transform = m_scene.ecs->componentManager->GetComponent<Transform>(light->GetOwner());
+            uint64_t id          = light->GetOwner();
+            std::hash<uint64_t> hasher;
+            uint64_t hashedId = hasher(id);
+            float angle     = glm::pi<float>() * (hashedId % 10) / 10.f;
+
+
+            float cosSquared  = glm::cos(angle) * glm::cos(angle);
+            float sinSquared = glm::sin(angle) * glm::sin(angle);
+            glm::vec3 direction(cosSquared, 0, sinSquared);
+            
+            transform->pos +=  s * direction * distance * (float)dt * speed;
+		}
 	}
-private:
-	EntityID m_entity;
+
 };
--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -21,6 +21,64 @@
 
 #include "TestSystem.hpp"
 
+
+void sponza_test(ECSEngine* ecs)
+{
+    Entity* camera = ecs->entityManager->CreateEntity();
+    Transform* t   = camera->AddComponent<Transform>();
+    camera->AddComponent<Camera>(90.f, 1920 / 1080.0f, 0.1f);
+    t->pos = {0.0f, 60.0f, -10.0f};
+    // t->rot = glm::rotate(t->rot, glm::radians(-90.f), glm::vec3(0,1,0));
+
+    Entity* e2                           = AssimpImporter::LoadFile("models/sponza.fbx", ecs);
+    e2->GetComponent<Transform>()->scale = {0.01f, 0.01f, 0.01f};
+
+    Entity* dlight                        = ecs->entityManager->CreateEntity();
+    dlight->GetComponent<NameTag>()->name = "DLight";
+    auto dl                               = dlight->AddComponent<DirectionalLight>();
+    dl->color                             = Color::White;
+    dl->intensity                         = 1.0f;
+
+
+    Entity* lightParent                        = ecs->entityManager->CreateEntity();
+    lightParent->GetComponent<NameTag>()->name = "point lights";
+    std::vector<Color> colors                  = {Color::Red, Color::Blue, Color::Green, Color::White};
+    constexpr int gridSize                     = 9;
+    for(int i = 0; i < gridSize; ++i)
+    {
+        for(int j = 0; j < gridSize; ++j)
+        {
+            Entity* d      = ecs->entityManager->CreateChild(lightParent);
+            auto l         = d->AddComponent<PointLight>();
+            int index      = i * gridSize + j;
+            l->color       = colors[index % colors.size()];
+            l->intensity   = 50.0f;
+            l->range       = 100.0f;
+            l->attenuation = {1, 0, 0};
+            // l->cutoff = glm::radians(40.f);
+            auto dt        = d->GetComponent<Transform>();
+            dt->pos        = {(i - gridSize / 2.f) * 25, 1.0f, (j - gridSize / 2.f) * 25};
+        }
+    }
+
+
+    // EntityID id2 = ecs->entityManager->CreateChild(id);
+    // ecs->componentManager->AddComponent<Mesh>(id2, vertices2, indices2);
+
+
+    /*
+    Transform* t2 = e2->GetComponent<Transform>();
+    t2->pos = { 0.f, 24.0f, 0.0f };
+    t2->scale = {0.1f, 0.1f, 0.1f};
+    e2->GetComponent<Transform>()->pos.y -= 200;
+    */
+    // t2->lPosition = {3.0f, -0.5f,-1.0f};
+    Transform* t2 = dlight->GetComponent<Transform>();
+    t2->rot       = glm::rotate(t2->rot, glm::radians(-45.f), glm::vec3(1, 0, 0));
+
+    ecs->systemManager->AddSystem<TestSystem>();
+}
+
 int main()
 {
 
@@ -31,60 +89,7 @@ int main()
 	ECSEngine* ecs = scene.ecs;
 	HierarchyUI hierarchyUi(&scene, editor.GetRenderer(), editor.GetMaterialSystem());
 
-
-
-	Entity* camera = ecs->entityManager->CreateEntity();
-	Transform* t = camera->AddComponent<Transform>();
-	camera->AddComponent<Camera>(90.f, 1920/1080.0f, 0.1f);
-	t->pos = { 0.0f, 60.0f, -10.0f };
-    //t->rot = glm::rotate(t->rot, glm::radians(-90.f), glm::vec3(0,1,0));
-
-    Entity* e2 = AssimpImporter::LoadFile("models/sponza.fbx", ecs);
-    e2->GetComponent<Transform>()->scale = {0.01f, 0.01f, 0.01f};
-
-	Entity* dlight = ecs->entityManager->CreateEntity();
-    dlight->GetComponent<NameTag>()->name = "DLight";
-	auto dl = dlight->AddComponent<DirectionalLight>();
-	dl->color = Color::White;
-	dl->intensity = 1.0f;
-
-
-
-    /*
-	Entity* lightParent = ecs->entityManager->CreateEntity();
-    lightParent->GetComponent<NameTag>()->name = "point lights";
-	for(int i = 0; i < 16; ++i)
-	{
-		for(int j = 0; j < 16; ++j)
-		{
-			Entity* d = ecs->entityManager->CreateChild(lightParent);
-			auto l = d->AddComponent<PointLight>();
-			l->color = Color::Red;
-			l->intensity = 100.0f;
-			l->range = 1000.0f;
-			l->attenuation = {1,1,1};
-			//l->cutoff = glm::radians(40.f);
-			auto dt = d->GetComponent<Transform>();
-			dt->pos = {i * 5, 1.0f , j*5};
-		}
-	}*/
-
-
-	//EntityID id2 = ecs->entityManager->CreateChild(id);
-	//ecs->componentManager->AddComponent<Mesh>(id2, vertices2, indices2);
-
-
-    /*
-	Transform* t2 = e2->GetComponent<Transform>();
-	t2->pos = { 0.f, 24.0f, 0.0f };
-	t2->scale = {0.1f, 0.1f, 0.1f};
-	e2->GetComponent<Transform>()->pos.y -= 200;
-    */
-	//t2->lPosition = {3.0f, -0.5f,-1.0f};
-	Transform* t2 = dlight->GetComponent<Transform>();
-    t2->rot = glm::rotate(t2->rot, glm::radians(-45.f), glm::vec3(1,0,0));
-
-	//ecs->systemManager->AddSystem<TestSystem>(lightParent->GetEntityID());
+    sponza_test(ecs);
 
 	editor.Run();
 
